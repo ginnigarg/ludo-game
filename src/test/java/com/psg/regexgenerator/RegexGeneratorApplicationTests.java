c9f699955364b8b743857406ab3b2ae56f39f828
ginni garg <ginnigarg@ginnis-MacBook-Pro.local>
2020-02-26 21:23:07 +0530

    Initial Commit

---
+++ src/test/java/com/psg/regexgenerator/RegexGeneratorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.psg.regexgenerator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RegexGeneratorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
